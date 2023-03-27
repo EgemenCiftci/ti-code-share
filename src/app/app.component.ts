b5eafd6a4fbd08e613267d4e2bb5c46b5bd46e10
Egemen Çiftci <egemenciftci@gmail.com>
2023-03-27 14:36:24 +0200

    First commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MonacoEditorConstructionOptions, MonacoEditorLoaderService, MonacoStandaloneCodeEditor } from '@materia-ui/ngx-monaco-editor';
+import { filter, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ti-code-share';
+  title = 'TI Code Share';
+  editorOptions: MonacoEditorConstructionOptions = {
+    language: 'html', // java, javascript, python, csharp, html, markdown, ruby
+    theme: 'vs-dark', // vs, vs-dark, hc-black
+    automaticLayout: true,
+  };
+  code = this.getCode();
+
+  constructor(private monacoLoaderService: MonacoEditorLoaderService) {
+    this.monacoLoaderService.isMonacoLoaded$
+      .pipe(
+        filter((isLoaded) => isLoaded),
+        take(1)
+      )
+      .subscribe();
+  }
+
+  getCode() {
+    return (
+      '<html><!-- // !!! Tokens can be inspected using F1 > Developer: Inspect Tokens !!! -->\n<head>\n	<!-- HTML comment -->\n	<style type="text/css">\n		/* CSS comment */\n	</style>\n	<script type="javascript">\n		// JavaScript comment\n	</' +
+      'script>\n</head>\n<body></body>\n</html>'
+    );
+  }
 }
