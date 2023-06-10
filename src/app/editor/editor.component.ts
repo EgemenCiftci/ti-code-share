0de4f00514c4e86cd21a0e08f20cf7076cfa4b42
Egemen Çiftci <egemenciftci@gmail.com>
2023-06-10 18:16:28 +0200

    .

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,38 +1,40 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { MonacoEditorConstructionOptions, MonacoStandaloneCodeEditor } from '@materia-ui/ngx-monaco-editor';
-import { Database, ref, set, onValue, DatabaseReference } from '@angular/fire/database';
+import { Database, ref, set, onValue, DatabaseReference, update, child, get } from '@angular/fire/database';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Languages } from '../enums/languages';
 import { Themes } from '../enums/themes';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, OnDestroy {
   languageEntries = Object.entries(Languages);
   themeEntries = Object.entries(Themes);
 
+  private _subscriptions: Subscription[] = [];
   private _defaultLanguage = this.languageEntries.find(f => f[1] == Languages.csharp)?.[0] ?? '';
   private _defaultTheme = this.themeEntries.find(f => f[1] == Themes.vs_dark)?.[0] ?? '';
   private _defaultCode = '';
   private _defaultPosition = { lineNumber: 1, column: 1 };
   private _defaultUserInfos = [{ name: '', position: this._defaultPosition }];
 
-  formGroup = new FormGroup({
+  formGroup: FormGroup = new FormGroup({
     userName: new FormControl({ value: localStorage.getItem('userName') ?? '', disabled: true }),
     key: new FormControl({ value: '', disabled: true }),
     language: new FormControl(this._defaultLanguage),
     theme: new FormControl(localStorage.getItem('theme') ?? this._defaultTheme),
-    code: new FormControl(this._defaultCode),
+    code: new FormControl(undefined)
   });
 
   editorOptions: MonacoEditorConstructionOptions = {
     language: this._defaultLanguage,
-    theme: this.formGroup.get('theme')?.value?.replace('_', '-'),
     automaticLayout: true,
+    theme: this.formGroup.get('theme')?.value?.replace('_', '-') ?? this._defaultTheme,
   };
 
   private _database: Database = inject(Database);
@@ -61,24 +63,40 @@ export class EditorComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.formGroup.get('language')?.valueChanges.subscribe(newValue => {
+    const languageControl = this.formGroup.get('language');
+
+    const s0 = languageControl?.valueChanges.subscribe(async newValue => {
       this.editorOptions = { ...this.editorOptions, language: newValue ?? this._defaultLanguage };
       if (this._languageRef) {
-        set(this._languageRef, newValue);
+        await set(this._languageRef, newValue);
       }
     });
 
-    this.formGroup.get('theme')?.valueChanges.subscribe(newValue => {
+    if (s0) {
+      this._subscriptions.push(s0);
+    }
+
+    const s1 = this.formGroup.get('theme')?.valueChanges.subscribe(newValue => {
       this.editorOptions = { ...this.editorOptions, theme: newValue?.replace('_', '-') ?? this._defaultTheme };
     });
 
-    this.formGroup.get('code')?.valueChanges.subscribe(newValue => {
-      if (this._codeRef) {
-        set(this._codeRef, newValue);
+    if (s1) {
+      this._subscriptions.push(s1);
+    }
+
+    const codeControl = this.formGroup.get('code');
+
+    const s2 = codeControl?.valueChanges.subscribe(async newValue => {
+      if (this._codeRef && newValue !== undefined) {
+        await set(this._codeRef, newValue);
       }
     });
 
-    this.route.paramMap.subscribe(params => {
+    if (s2) {
+      this._subscriptions.push(s2);
+    }
+
+    const s3 = this.route.paramMap.subscribe(async params => {
       const key = params.get('key');
 
       if (!localStorage.getItem('userName')) {
@@ -91,16 +109,36 @@ export class EditorComponent implements OnInit {
         return;
       }
 
-      this.formGroup.get('key')?.setValue(key);
+      this.formGroup.get('key')?.patchValue(key);
 
       this._languageRef = ref(this._database, `${key}/language`);
       this._codeRef = ref(this._database, `${key}/code`);
       this._userInfosRef = ref(this._database, `${key}/userInfos`);
 
-      onValue(this._languageRef, f => this.formGroup.get('language')?.setValue(f.val() ?? this._defaultLanguage));
-      onValue(this._codeRef, f => this.formGroup.get('code')?.setValue(f.val() ?? this._defaultCode));
+      codeControl?.patchValue(await this.getCode());
+
+      onValue(this._languageRef, f => {
+        if (languageControl && f.val() !== languageControl.value) {
+          languageControl.patchValue(f.val() ?? this._defaultLanguage);
+        }
+      });
+
+      onValue(this._codeRef, f => {
+        if (codeControl && f.val() !== codeControl.value) {
+          codeControl.patchValue(f.val() ?? this._defaultCode);
+        }
+      });
+
       onValue(this._userInfosRef, f => this.userInfos = f.val() ?? this._defaultUserInfos);
     });
+
+    if (s3) {
+      this._subscriptions.push(s3);
+    }
+  }
+
+  ngOnDestroy() {
+    this._subscriptions.forEach(s => s.unsubscribe());
   }
 
   editorInit(editor: MonacoStandaloneCodeEditor) {
@@ -119,5 +157,8 @@ export class EditorComponent implements OnInit {
   private getUserInfo(userName: string): { name: string, position: { lineNumber: number, column: number } } | undefined {
     return this.userInfos.find(f => f.name === userName);
   }
-}
 
+  private async getCode(): Promise<string> {
+    return (await get(this._codeRef!)).val() ?? this._defaultCode;
+  }
+}
